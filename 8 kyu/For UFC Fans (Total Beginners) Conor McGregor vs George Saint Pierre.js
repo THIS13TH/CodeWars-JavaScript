c1d202312c89bc42381c172a0bf38870e7a04216
Alexander Malyutin <this13th@yandex.ru>
2023-07-05 20:52:29 +0600

    8 kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ 8 kyu/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,4 @@
+var quote = function(fighter) {
+    if(fighter.toLowerCase() === "george saint pierre") return "I am not impressed by your performance.";
+    else return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
